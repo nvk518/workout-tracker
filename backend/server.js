d9cf494c8dfe58eee2ef7be2b931ed24072a0a9e
nvk518 <nvk5181@gmail.com>
2024-08-01 14:48:27 -0700

    cors fix

--- backend/server.js
+++ backend/server.js
@@ -14,6 +14,7 @@ app.use(bodyParser.json());
 
 const allowedOrigins = [
   process.env.FRONTEND_URL || 'http://localhost:1344',
+  'https://www.gymkhanna.space',
   'https://gymkhanna.space',
 ];
 
